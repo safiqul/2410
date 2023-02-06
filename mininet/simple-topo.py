93b07620466c0597e02191422f12329c9509fd75
Safiqul Islam <safiquli@ifi.uio.no>
2023-02-06 14:29:58 +0100

    mininet simple topo added

---
+++ mininet/simple-topo.py
@@ -0,0 +1,43 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Node
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+from mininet.link import TCLink
+
+
+class LinuxRouter( Node ):
+    """A Node with IP forwarding enabled.
+    Means that every packet that is in this node, comunicate freely with its interfaces."""
+
+    def config( self, **params ):
+        super( LinuxRouter, self).config( **params )
+        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
+
+    def terminate( self ):
+        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
+        super( LinuxRouter, self ).terminate()
+
+
+class NetworkTopo( Topo ):
+
+    def build( self, **_opts ):		
+        r1=self.addHost("r1",ip=None)
+        r2=self.addNode("r2",cls=LinuxRouter,ip=None)
+        r3=self.addHost("r3",ip=None)
+        self.addLink(r1,r2,params1={ 'ip' : '10.0.0.1/24' },params2={ 'ip' : '10.0.0.2/24' }, bw=10, delay='25ms')#,max_queue_size=834, use_htb=True)
+        self.addLink(r2,r3,params1={ 'ip' : '10.0.1.1/24' },params2={ 'ip' : '10.0.1.2/24' }, bw=10, delay='25ms')#, max_queue_size=834, use_htb=True)
+
+topo = NetworkTopo()
+net = Mininet( topo=topo, link=TCLink )
+net.start()
+
+#ip route add ipA via ipB dev INTERFACE
+#every packet going to ipA must first go to ipB using INTERFACE
+net["r1"].cmd("ip route add 10.0.1.2 via 10.0.0.2 dev r1-eth0")
+net["r3"].cmd("ip route add 10.0.0.1 via 10.0.1.1 dev r3-eth0")
+#this command is just to r3 ping r2 work, because it will use the correct ip
+net["r3"].cmd("ip route add 10.0.0.2 via 10.0.1.1 dev r3-eth0")
+net.pingAll()
+CLI( net )
+net.stop()
